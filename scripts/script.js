5706af50c5dbe6acb08502194d2766cc89a15fb7
NoahGAP <willianandscully@gmail.com>
2024-07-19 16:15:51 -0500

    menu responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,28 @@
 const yearSelector = document.getElementById("current-year");
+const menu = document.getElementById("menu");
+const toogleButton = document.getElementById("menu-icon");
 
 const getcurrentYear = () => {
     const fechaActual = new Date();
     return fechaActual.getFullYear();
 }
 
+const setMenu = () => {
+    let width = window.innerWidth;
+    if (width < 780) {
+        menu.classList.add('closed');
+    } else {
+        menu.classList.remove('closed');
+    }
+}
+
+const toogleMenuButton = () => {
+    menu.classList.toggle('closed');
+}
+
 yearSelector.innerText = getcurrentYear();
+
+toogleButton.addEventListener('click', toogleMenuButton)
+window.addEventListener('resize', setMenu);
+
+setMenu()
